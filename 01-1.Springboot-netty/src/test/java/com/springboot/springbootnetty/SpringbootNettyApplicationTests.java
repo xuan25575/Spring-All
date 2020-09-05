34f9ab21bcdff1fe22a7c33908f6c4a84606efe6
huang <xuanyu.huang@hand-china.com>
2020-09-05 18:01:47 +0800

    1.spring boot整合netty 心跳机制

---
+++ 01-1.Springboot-netty/src/test/java/com/springboot/springbootnetty/SpringbootNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootnetty;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
